5c719723811a36d68eddc698aec2231af8bb5793
Hamza khan <150890458+Hamja32@users.noreply.github.com>
2024-03-26 16:05:07 +0530

    This is a simple calculator using js

---
+++ index.js
@@ -0,0 +1,37 @@
+(function () {
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let btnequal = document.querySelector('.btnequal');
+    
+    buttons.forEach(function(button){
+        button.addEventListener('click',function(e){
+            let value = e.target.dataset.num;
+            screen.value  += value;
+        })
+
+    });
+
+    btnequal.addEventListener('click',function(e) {
+
+        if(screen.value === '') {
+            screen.value = "Please enter";
+        } else {
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
+    })
+        clear.addEventListener('click',function(e){
+            screen.value = "";
+        })
+})();
+
+
+function bgcolor (){
+    let color = prompt("Enter the your favorite color : ");
+    document.body.style.backgroundColor = color;
+}
+
+
+
+let color;
